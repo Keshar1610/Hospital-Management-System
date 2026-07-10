1fb68d3b8928742d60d22c2633cb76e4edc7951c
Keshar1610 <kesharchoudhary113@gmail.com>
2026-07-10 22:44:18 +0530

    Update main.py

--- HospitalManagement-master/main.py
+++ HospitalManagement-master/main.py
@@ -1,6 +1,9 @@
-from application import app
-
+import os
 from application import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
